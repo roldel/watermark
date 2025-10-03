bc5b6c7a88d62db53d5589dad7e50346dc412af0
roldel <roldel@protonmail.com>
2025-10-03 19:46:29 +0000

    Add depencies install commands

--- init-command.sh
+++ init-command.sh
@@ -1,3 +1,11 @@
 # Initialize docker container
 
 sudo docker run -it -v $(pwd):/script -w /script python:slim bash
+
+# apt dependencies
+
+apt update && apt install -y ghostscript libjpeg-dev zlib1g-dev
+
+# python dependencies
+
+pip install pillow img2pdf PyPDF2 reportlab
